50abab266d389c231656ade39bab6c78e06056c2
AmitSharma1094 <amit.sh1094@gmail.com>
2016-07-31 19:46:38 +0530

    ipso-illuminance & ipso-hum.c added and Makefile.ipso-objects modified

--- apps/ipso-objects/ipso-objects.c
+++ apps/ipso-objects/ipso-objects.c
@@ -53,17 +53,17 @@ ipso_objects_init(void)
 #endif
 
 #if PLATFORM_HAS_BUTTON
-  ipso_button_init();
+//  ipso_button_init();
 #endif
 
 #ifdef IPSO_LIGHT_CONTROL
-  ipso_light_control_init();
+//  ipso_light_control_init();
 #elif PLATFORM_HAS_LEDS
-  ipso_leds_control_init();
+//  ipso_leds_control_init();
 #endif
 
 #ifdef IPSO_3311
-  ipso_switch_control_init();
+  ipso_light_control_init();
 #endif
 
 #ifdef IPSO_HUMIDITY
